448b392b46177a1e7d7aca449e3df7760b60be34
Yazdani Chowdhury <yaz4dani@gmail.com>
2023-02-27 00:24:15 +0200

    WIP

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import style from "./About.module.scss";
+import HomePageLayout from "../../layouts/HomePageLayout";
+import CardLayout from "../../components/CardLayout/CardLayout";
+const About = () => {
+  return (
+    <HomePageLayout>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-6 col-lg-6">
+            <CardLayout>
+              <h5>Image will be here</h5>
+            </CardLayout>
+          </div>
+
+          <div className="col-xl-6 col-lg-6">
+            <CardLayout>
+              <h5>
+                Hi, I am yazdani chowdhury. I work as a full stack developer.
+              </h5>
+            </CardLayout>
+          </div>
+        </div>
+      </div>
+    </HomePageLayout>
+  );
+};
+
+export default About;
